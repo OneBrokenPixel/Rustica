5e7e4678f1dc6be871056f878b679dc2394fc65f
Rieya <rieya.online@gmail.com>
2014-07-30 20:42:12 +0100

    scrollpanel class done

--- src/scrollpanel.js
+++ src/scrollpanel.js
@@ -2,25 +2,25 @@ Rustica = {};
 Rustica.UI = {};
 
 Rustica.UI.Scrollpanel = function (game, x, y, width, height, backgroundImg, maskImg, trackImg, gripImg) {
-    var game = game;
+    Phaser.Group.call(this, game);
 
-    //this.content = game.add.group();
     this._minGripSize = 30;
     this._maxGripSize = height - 10;
+    this._trackHeight = 0;
+    this._trackY = 5;
+    this._dragRatio = 0;
 
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
-    this.contentHeight = 10;
-
-    this._images = game.add.group();
-    this._images.x = x;
-    this._images.y = y;
+    this.w = width;
+    this.h = height;
+    this.contentHeight = 20;
 
     this.content = game.add.group();
-    this.content.x = x;
-    this.content.y = y;
+    this.add(this.content);   // make Scrollpanel this.content's parent
+    this.content.x = 0;
+    this.content.y = 0;
+
 
     if (backgroundImg) {
         backgroundImg.x = 0;
@@ -28,77 +28,87 @@ Rustica.UI.Scrollpanel = function (game, x, y, width, height, backgroundImg, mas
         backgroundImg.width = width;
         backgroundImg.height = height;
 
-        this._images.add(backgroundImg);
+        this.add(backgroundImg);
     }
     
     
-    this._track = trackImg || new Phaser.Image(game, 0, 0, "ui", "grey_sliderVertical.png");
-    game.add.existing(this._track);
-    this._track.height = height - 10;
-    this._track.x = x + width - 10;
-    this._track.y = y + 10/2;
-    
-    
-    this.grip = gripImg || new Phaser.Image(game, 0, 0, "grip");
-    game.add.existing(this.grip);
-    this.grip.x = this._track.x - this.grip.width - 3;
-    this.grip.y = this._track.y;
-    this.grip.height = this._track.height;
+    var track = trackImg || this.create(0, 0, "ui", "grey_sliderVertical.png");
+    track.height = height - 10;
+    track.x = width - 10;
+    track.y = this._trackY;
+   
+    this._trackHeight = track.height;
+
+    this.grip = gripImg || this.create(0, 0, "grip"); 
+    this.grip.x = track.x - this.grip.width - 3;
+    this.grip.y = track.y;
+    this.grip.height = this._trackHeight;
 
     this.grip.inputEnabled = true;
     this.grip.input.enableDrag(false, false, false, 255,
                                new Phaser.Rectangle(this.grip.x, this.grip.y, 
-                                                    this.grip.width, this._track.height));
+                                                    this.grip.width, this._trackHeight));
     this.grip.input.allowHorizontalDrag = false;
-
+    
     
     var mask = new PIXI.Graphics();
     mask.beginFill();
-    mask.drawRect(x-1, y+1, width+1, height-2);
+    mask.drawRect(x, y, width, height);
     mask.endFill();
-    this.content.mask = mask;
+    this.mask = mask;
     
-
     return this;
 };
 
+Rustica.UI.Scrollpanel.prototype = Object.create(Phaser.Group.prototype);
 Rustica.UI.Scrollpanel.prototype.constructor = Rustica.UI.Scrollpanel;
 
-Rustica.UI.Scrollpanel.prototype = {
-    add: function(items) {
-        var i, item;
-        for (i=0; i < arguments.length; i++) {
-            item = arguments[i];
+Rustica.UI.Scrollpanel.prototype.addItem =  function (item) {
+    
+    // add item to content group
+    this.content.add(item);
+    item.x = 20;
+    item.y = this.contentHeight;
+    this.contentHeight += item.height + 10;
 
-            this.content.add(item);
-            item.x = 20;
-            items.y = this.contentHeight;
-            this.contentHeight += item.height + 10;
-        }
+    // resize the grip
+    var windowContentRatio = this.h / this.contentHeight;
+    this.grip.height = this._trackHeight * windowContentRatio;
+
+    this.grip.height = this.grip.height > this._maxGripSize ? this._maxGripSize : this.grip.height;
+    this.grip.height = this.grip.height < this._minGripSize ? this._minGripSize : this.grip.height;
         
-        this.contentHeight += 10;
-
-        // resize the grip
-        var windowContentRatio = this.height / this.contentHeight;
-        this.grip.height = this._maxGripSize * windowContentRatio;
-    },
-
-    remove: function(item) {
-        console.log("unimplemented");
-    },
-
-    move: function(newX, newY) {
-        console.log("unimplemented");
-    },
-
-    update: function() {
-        if (this.grip.input.isDragged) {
-            var windowScrollAreaSize = this.contentHeight - this.height;
-            var trackScrollAreaSize = this._track.height - this.grip.height;
-            var dragRatio = windowScrollAreaSize / trackScrollAreaSize;
-            this.content.y = this._track.y - ((this.grip.y-this._track.y) * dragRatio);
-        }
-    }
-
-
+    // calculate scroll amount
+    var windowScrollAreaSize = this.contentHeight - this.h;
+    var trackScrollAreaSize = this._trackHeight - this.grip.height;
+    this._dragRatio = windowScrollAreaSize / trackScrollAreaSize;
+    
 };
+
+Rustica.UI.Scrollpanel.prototype.removeItem = function (item) {
+    console.log("unimplemented");
+};
+
+
+Rustica.UI.Scrollpanel.prototype.move = function (newX, newY) {
+    this.x = newX;
+    this.y = newY;
+
+    this.mask = null;
+
+    // redraw mask
+    var mask = new PIXI.Graphics();
+    mask.beginFill();
+    mask.drawRect(this.x, this.y, this.w, this.h);
+    mask.endFill();
+
+    this.mask = mask;
+};
+
+Rustica.UI.Scrollpanel.prototype.update = function () {
+    if (this.grip.input.isDragged && this.grip.height < this._maxGripSize) {
+        // move content group upwards by drag ratio
+        this.content.y = 0 - (this.grip.y-this._trackY) * this._dragRatio;
+    }
+};
+
--- src/uitest.js
+++ src/uitest.js
@@ -32,38 +32,6 @@ var dragRatio;
 
 function create() {
     game.stage.backgroundColor = "#FFFFFF";
-
-    //var background = game.add.image(game.width/2, game.height/2, "background");
-    //background.anchor.setTo(0.5, 0.5);
-
-    panel = game.add.image(20,25, "panel");
-    
-    panelGroup = game.add.group();
-    panelGroup.x = panel.x;
-    panelGroup.y = panel.y;
-
-    scrollLine = game.add.image(0, 0, "ui", "grey_sliderVertical.png");
-    scrollLine.height = panel.height - 10;
-    scrollLine.x = panel.x + panel.width - 10;
-    scrollLine.y = panel.y + 10/2;
-
-    //scrollCircle = game.add.image(0,0,"ui", "grey_sliderEnd.png");
-    //scrollCircle.width *= 2.2;
-    //scrollCircle.height *= 2.2;
-    scrollCircle = game.add.image(0,0,"grip");
-    scrollCircle.x = scrollLine.x - scrollCircle.width - 3;
-    scrollCircle.y = scrollLine.y;
-    
-    var i, y=10;
-    for (i=0; i < 100; i++) {
-        var button = new Phaser.Button(game, 15, y, "ui", buttonDown, this, 
-                                       "grey_button00.png", "grey_button00.png", 
-                                       "grey_button00.png", "grey_button00.png");
-        button.width *= 0.85;
-        panelGroup.add(button);
-        y += button.height + 10;
-    }
-    y += 10;
     
     /*
     panelMask = game.add.image(0,0,"panelMask");
@@ -73,47 +41,25 @@ function create() {
     panelMask.z = 1;
     */
 
-    
-    var myMask = new PIXI.Graphics();
-    myMask.beginFill();
-    myMask.drawRect(panel.x-1, panel.y+1, panel.width+1, panel.height-2);
-    myMask.endFill();
-    panelGroup.mask = myMask;
 
-    scrollCircle.inputEnabled = true;
-    scrollCircle.input.enableDrag(false, false, false, 255, 
-                                  new Phaser.Rectangle(scrollCircle.x, scrollCircle.y, 
-                                                       scrollCircle.width, scrollLine.height));
-    scrollCircle.input.allowHorizontalDrag = false;
+    var panel = new Phaser.Image(game, 0,0, "panel");
+    this.scrollpanel = new Rustica.UI.Scrollpanel(game, 300, 25, panel.width, panel.height, panel);
 
-
-    contentSize = y;
-    windowSize = panel.height;
-    trackSize = scrollLine.height;
-    windowContentRatio = windowSize/contentSize;
-
-    scrollCircle.height = trackSize * windowContentRatio;
-    windowScrollAreaSize = contentSize - windowSize;
-    trackScrollAreaSize = trackSize - scrollCircle.height; 
-
-    dragRatio = windowScrollAreaSize / trackScrollAreaSize;
-
-    this.scrollpanel = new Rustica.UI.Scrollpanel(game, 300, 25, panel.width, panel.height, 
-                                                 new Phaser.Image(game, 0, 0, "panel"));
-
-    for (i=0; i<20; i++) {
-        var button = new Phaser.Button(game, 15, y, "ui", buttonDown, this, 
+    for (i=0; i<100; i++) {
+        var button = new Phaser.Button(game, 0, 0, "ui", buttonDown, this, 
                                        "grey_button00.png", "grey_button00.png", 
                                        "grey_button00.png", "grey_button00.png");
         button.width *= 0.85;
-        this.scrollpanel.add(button);
+        this.scrollpanel.addItem(button);
     }
     
+
+
 }
 
 function update() {
-    panelGroup.y = scrollLine.y - ((scrollCircle.y-scrollLine.y) * dragRatio);
-    this.scrollpanel.update();
+
+    //this.scrollpanel.update();
 }
 
 function render() {
