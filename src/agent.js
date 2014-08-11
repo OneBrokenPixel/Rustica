3a482905628971bf8fd6a67fefb9afb33a2ff52a
Anthony Todd <toddy201@gmail.com>
2014-08-11 13:38:10 +0100

    Adding scroll panel to main.

---
+++ src/agent.js
@@ -0,0 +1,45 @@
+Rustica = {};
+Rustica.Game = {};
+
+// Agent Constuctor
+Rustica.Game.Agent = function(name, age, efficency) {
+
+	    this.name = name;
+	    this.age = age;
+	    this.efficency = efficency;
+	    this.timeIdle = 0.0;
+	    this.timeWorking = 0.0;
+    
+    return this;
+}
+
+Rustica.Game.Agent.prototype.constructor = Rustica.Game.Agent;
+
+
+Rustica.Game.AgentTaskQueue = function() {
+	
+	this.agentsWorking = [];
+	this.agentsIdle = [];
+}
+
+
+Rustica.Game.AgentTaskQueue.prototype.constructor = Rustica.Game.AgentTaskQueue;
+
+Rustica.Game.AgentTaskQueue.prototype.findAgent = function( agent ){
+	
+	var index = this.agentsWorking.indexOf(agent);
+
+	return index == -1 ? this.agentsIdle.indexOf(agent) : index;
+}
+
+
+Rustica.Game.AgentTaskQueue.prototype.addAgent = function( agent ) {
+	
+	var searchResult = this.findAgent( agent );
+	
+	if( searchResult == -1 ){
+		this.agentsIdle.push(agent);
+	}
+	
+}
+
--- src/game.js
+++ src/game.js
@@ -9,6 +9,15 @@ function preload() {
     game.scale.refresh();
     
     game.load.image('lammas', 'assets/Lammas.jpg');
+    
+    game.load.atlasXML("ui",  "assets/ui/greySheet.png", "assets/ui/greySheet.xml" );
+    game.load.image("background", "assets/ui/background.png");
+    game.load.image("panel", "assets/ui/panel.png");
+    game.load.image("panelMask", "assets/ui/panelMask.png");
+    game.load.image("grip", "assets/ui/grip.png.");
+
+    game.load.image("hline", "assets/ui/hline.png");
+    game.load.image("tab", "assets/ui/tab.png");
 
 }
 
@@ -19,26 +28,40 @@ function create() {
 	
 	lammas.scale.x = 2
 	lammas.scale.y = 2
-	
+		/*
 	crop = cropModule.getCrop("Wheat");
 
 	cropModule.addField()
 	cropModule.getField(0).resize(9);
-	cropModule.getField(0).plant(crop,game.time.totalElapsedSeconds());
+	cropModule.getField(0).plant(crop,game.time.totalElapsedSeconds());*/
+	
+    var panel = new Phaser.Image(game, 0,0, "panel");
+    
+    this.scrollpanel = new Rustica.UI.Scrollpane(game, 20, 50, panel.width, panel.height, panel);
+
+    var button = new Phaser.Button(game, 0, 0, "ui", buttonDown, this, 
+                                   "grey_button00.png", "grey_button00.png", 
+                                   "grey_button00.png", "grey_button00.png");
+    button.width *= 0.85;
+    this.scrollpanel.addItem(button);
+
 }
 
 function update() {
-	
+	/*
 	if( cropModule.getField(0).isHarvestTime( game.time.totalElapsedSeconds() ) ) {
 		profit = profit + cropModule.getField(0).harvest();
 		cropModule.getField(0).plant(crop,game.time.totalElapsedSeconds());
 		
 		console.log("Wheat Harvested: " + profit);
 	}
-	
+	*/
 	
 }
 
 function render() {
 }
 
+function buttonDown() {
+    console.log("pressed");
+}
